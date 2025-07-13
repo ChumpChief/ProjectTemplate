209f165432d6d7a66bd4e806b3f51f5f3e2ba48b
Matt Rakow <matt.rakow@gmail.com>
2025-07-13 16:13:50 -0700

    Update example structure

--- examples/src/package2-example/index.ts
+++ examples/src/package2-example/index.ts
@@ -3,13 +3,9 @@ import { createRoot } from "react-dom/client";
 
 import { AppView } from "./view/index.js";
 
-const start = (): void => {
-    const appViewDiv = document.createElement("div");
-    const reactRoot = createRoot(appViewDiv);
-    reactRoot.render(createElement(AppView));
+const appRootDiv = document.createElement("div");
+appRootDiv.classList.add("app-root");
+const reactRoot = createRoot(appRootDiv);
+reactRoot.render(createElement(AppView));
 
-    const div = document.getElementById("content") as HTMLDivElement;
-    div.append(appViewDiv);
-};
-
-start();
+document.body.append(appRootDiv);
